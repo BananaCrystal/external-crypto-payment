c96b9f5124a064bd7c0cc191dac5fff36f0d3dc2
bob <162284281+Brainydel@users.noreply.github.com>
2025-05-23 11:32:52 +0100

    dev-fixes

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  // Disable image optimization since it requires server components
-  images: {
-    unoptimized: true,
-  },
-  // Ensures that any assets referenced in your app use relative paths
-  assetPrefix: '',
-  // Disables features that won't work with static export
-  trailingSlash: true,
-  // Ensures correct behavior for client-side routing
-  reactStrictMode: true,
+    output: 'export',
 };
 
 export default nextConfig;
